3dcc6bb8b885142bb8d8aa609ce433fc34525f0a
Bryan Wodi <talk2kamp@gmail.com>
2018-02-06 15:42:28 -0600

    Update config.fish
    included shortcuts in private folder for secret aliases/functions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,10 @@ for file in aliases linux
     source ~/.config/fish/includes/$file.fish
 end
 
+for file in private
+    source ~/.config/fish/private/$file.fish
+end
+
 switch (uname)
 case Linux
 	set -gx PATH /opt/idea-IU-171.4249.39/bin $PATH
