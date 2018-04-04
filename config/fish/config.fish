84959b22d6e40c6d92a9392ccbd060758b9d3f46
Bryan Wodi <talk2kamp@gmail.com>
2018-04-04 13:02:17 -0500

    removed redundant private load

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,10 +2,6 @@ for file in aliases linux private_aliases
     source ~/.config/fish/includes/$file.fish
 end
 
-for file in private
-    source ~/.config/fish/private/$file.fish
-end
-
 switch (uname)
 case Linux
 	set -gx PATH /opt/idea-IU-171.4249.39/bin $PATH
