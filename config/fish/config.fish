c79c3b2efc44e8b36cbf7cfddcb1159e0eaf7584
Bryan Wodi <2839540+talk2bryan@users.noreply.github.com>
2019-11-21 00:59:42 -0600

    moved cuda and idea to private folder

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,3 @@
 for file in aliases linux private_aliases
     source ~/.config/fish/includes/$file.fish
 end
-
-switch (uname)
-case Linux
-	set -gx PATH /opt/idea-IU-171.4249.39/bin $PATH
-	set -gx PATH /usr/local/cuda-9.0/bin $PATH
-end
