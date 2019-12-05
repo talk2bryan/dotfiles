e31d4dacba17f83b7b84eb711c460523464b59a9
Bryan Wodi <2839540+talk2bryan@users.noreply.github.com>
2019-12-05 15:24:33 -0600

    added the file name arg

--- config/fish/functions/subl.fish
+++ config/fish/functions/subl.fish
@@ -1,8 +1,8 @@
 function subl -d 'Open Sublime Text'
   switch (uname)
       case Linux
-        /opt/sublime_text_3/sublime_text
+        /opt/sublime_text_3/sublime_text $argv
       case Darwin
-        /usr/local/bin/subl
+        /usr/local/bin/subl $argv
       end
 end
