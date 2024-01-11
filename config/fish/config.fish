40a8c78cc96f11ce01d5ed3e419e4a60848bed67
Bryan Wodi <12839540+talk2bryan@users.noreply.github.com>
2024-01-11 04:01:00 -0600

    init pyenv and activate functions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,5 +2,11 @@ for file in aliases linux  mac private_aliases
     source ~/.config/fish/includes/$file.fish
 end
 
+for file in ~/.config/fish/functions/*
+    source $file
+
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
+
 direnv hook fish | source
 
